8b1e853cd3586ff42ca1ab373c24d09c09125c8a
Cmarcos2 <qcsmarcos@tip.edu.ph>
2024-05-19 10:33:29 +0800

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -83,7 +83,12 @@ st.markdown('<div class="names main">Christian Marcos | Ji Han Gang | May 19, 20
 # File uploader with a placeholder for the message
 st.markdown('<div class="uploader main">Choose a weather photo to predict if it is Shine, Cloudy, Sunrise, or Rain:</div>', unsafe_allow_html=True)
 upload_placeholder = st.empty()  # Placeholder for the message
-file = st.file_uploader("", type=["jpg", "png"], key="file_uploader", label_visibility="collapsed", container_class="file-uploader")
+
+# Wrap the file uploader inside a div and assign a custom class
+file_uploader_div = st.empty()
+file_uploader_div.markdown('<div class="file-uploader">', unsafe_allow_html=True)
+file = st.file_uploader("", type=["jpg", "png"], key="file_uploader", label_visibility="collapsed")
+file_uploader_div.markdown('</div>', unsafe_allow_html=True)
 
 def import_and_predict(image_data, model):
     size = (150, 150)
