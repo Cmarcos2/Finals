2e9cebb6e91aac0a7dce6df0b505bc4f0e415616
Cmarcos2 <qcsmarcos@tip.edu.ph>
2024-05-19 10:52:20 +0800

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -81,7 +81,7 @@ st.markdown('<div class="header main">Final Examination: Weather Classification 
 st.markdown('<div class="names main">Christian Marcos | Ji Han Gang | May 19, 2024</div>', unsafe_allow_html=True)
 
 # File uploader with a placeholder for the message
-st.markdown('<div class="uploader main">A Weather Photo to Predict if it is Shine, Cloudy, Sunrise, or Rain</div>', unsafe_allow_html=True)
+st.markdown('<div class="uploader main">A Weather Photo Can Predict if it is Shine, Cloudy, Sunrise, or Rain</div>', unsafe_allow_html=True)
 upload_placeholder = st.empty()  # Placeholder for the message
 
 # Wrap the file uploader inside a div and assign a custom class
